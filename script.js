405e32324e435469ff5e0aff0cacf00eb387b6a3
Harsh Pratap Singh <123838518+harshu0001@users.noreply.github.com>
2023-06-23 20:03:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-const wrapper =document.querySelector('.wrapper');
-const loginLink =document.querySelector('.login-link');
-const registerLink =document.querySelector('.register-link');
-const btnPopup =document.querySelector('.btnLogin-popup');
-const iconClose =document.querySelector('.icon-close');
+let text = document.getElementById('text');
+let gateLeft = document.getElementById('gate-left');
+let gateRight = document.getElementById('gate-right');
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
 
-registerLink.addEventListener('click',()=>{
-    wrapper.classList.add('active');
+    text.style.marginTop= value*2.5 + 'px';
+    gateLeft.style.left= value*0.5 + 'px';
+    gateRight.style.left= value * -0.5 + 'px';
 });
-
-
-
-loginLink.addEventListener('click',()=>{
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click',()=>{
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click',()=>{
-    wrapper.classList.remove('active-popup');
-});
-
