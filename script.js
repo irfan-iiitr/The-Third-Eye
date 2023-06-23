15983e822ebb9df161493f40dc542466f125e079
Prakharpandey007 <124185531+Prakharpandey007@users.noreply.github.com>
2023-06-23 20:12:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let text = document.getElementById('text');
-let gateLeft = document.getElementById('gate-left');
-let gateRight = document.getElementById('gate-right');
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
+const wrapper =document.querySelector('.wrapper');
+const loginLink =document.querySelector('.login-link');
+const registerLink =document.querySelector('.register-link');
+const btnPopup =document.querySelector('.btnLogin-popup');
+const iconClose =document.querySelector('.icon-close');
 
-    text.style.marginTop= value*2.5 + 'px';
-    gateLeft.style.left= value*0.5 + 'px';
-    gateRight.style.left= value * -0.5 + 'px';
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
 });
+
+
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+});
+
