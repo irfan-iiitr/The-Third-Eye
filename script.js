27a6535d21d9db22c7a1c7facba7385e49851940
Harsh Pratap Singh <123838518+harshu0001@users.noreply.github.com>
2023-06-24 14:52:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let text = document.getElementById('text');
+let gateLeft = document.getElementById('gate-left');
+let gateRight = document.getElementById('gate-right');
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    text.style.marginTop= value*2.5 + 'px';
+    gateLeft.style.left= value*0.5 + 'px';
+    gateRight.style.left= value * -0.5 + 'px';
+});
 const wrapper =document.querySelector('.wrapper');
 const loginLink =document.querySelector('.login-link');
 const registerLink =document.querySelector('.register-link');
